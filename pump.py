c714ea572c39f6e80acfe8db7e4447d0c5b0d252
rochana <rochana.atapattu2@gmail.com>
2019-07-19 00:03:53 +0530

    sensors

--- pump.py
+++ pump.py
@@ -1,7 +1,13 @@
 import RPi.GPIO as GPIO
 import gpiopins
+from hcsr04sensor.sensor import Measurement
+import Adafruit_DHT
 
-GPIO.setmode(GPIO.BOARD)
+GPIO_MODE = GPIO.BOARD
+DHT22 = Adafruit_DHT.DHT22
+
+
+GPIO.setmode(GPIO_MODE)
 
 def turnOn():
     
@@ -15,3 +21,18 @@ def turnOff():
 
 def cleanUp():
     GPIO.cleanup()
+
+def distance():
+    temp = tempurature()
+    value = Measurement(gpiopins.TRIGGER_PIN, gpiopins.ECHO_PIN, temp, "metric", gpio_mode=GPIO_MODE)
+    return value
+
+def tempurature():
+    humidity,temp = Adafruit_DHT.read_retry(DHT22, gpiopins.DHT22_BCM_PIN)
+
+    if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+    print('Failed to get reading. Try again!')
+
+    return(humidity,temp)
