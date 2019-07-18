0e23f6c77ab5da09db053ecda4d1e49846803b72
rochana <rochana.atapattu2@gmail.com>
2019-07-19 00:35:44 +0530

    sensors

--- pump.py
+++ pump.py
@@ -30,8 +30,8 @@ def distance():
 def tempurature():
     humidity,temp = Adafruit_DHT.read_retry(DHT22, gpiopins.DHT22_BCM_PIN)
 
-    if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    if humidity is not None and temp is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, humidity))
     else:
     print('Failed to get reading. Try again!')
 
