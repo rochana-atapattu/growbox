4d0560722e66554dbb3272a3b4cc22128c17635a
rochana-atapattu <rochana.atapattu2@gmail.com>
2019-07-28 19:30:42 +0100

    controls

--- Output.py
+++
@@ -1,7 +0,0 @@
-import IO
-
-class Output(IO):
-
-    def __init__(self,pin):
-        super.__init__()
-        GPIO.setup(pin, GPIO.OUT) 
--- pump.py
+++ pump.py
@@ -1,57 +1,53 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+import gpiozero as gpio
 import gpiopins
 from hcsr04sensor.sensor import Measurement
 import Adafruit_DHT
 
-GPIO_MODE = GPIO.BOARD
+# GPIO_MODE = GPIO.BOARD
 DHT22 = Adafruit_DHT.DHT22
 
+pump_up_relay = gpio.OutputDevice(gpiopins.PUMP_UP_PIN)
+pump_mix_relay = gpio.OutputDevice(gpiopins.PUMP_MIX_PIN)
+grow_light_relay = gpio.OutputDevice(gpiopins.LED_PIN)
+solenoid_valve_relay = gpio.OutputDevice(gpiopins.SOLENOID_PIN)
 
-GPIO.setmode(GPIO_MODE)
+distance_sensor = gpio.DistanceSensor(gpiopins.ECHO_PIN,gpiopins.TRIGGER_PIN,max_distance=gpiopins.MAX_DISTANCE)
 
-def turnOn():
-    
-    GPIO.setup(gpiopins.PUMP_UP_PIN, GPIO.OUT) 
-    GPIO.output(gpiopins.PUMP_UP_PIN, GPIO.LOW)
-    print("pumpOn")
-    
+temp_controller_fan = gpio.PWMOutputDevice()
 
-def turnOff():
-    GPIO.setup(gpiopins.PUMP_UP_PIN, GPIO.OUT) 
-    GPIO.output(gpiopins.PUMP_UP_PIN, GPIO.HIGH)
 
-def drainOn():
-    
-    GPIO.setup(gpiopins.SOLENOID_PIN, GPIO.OUT) 
-    GPIO.output(gpiopins.SOLENOID_PIN, GPIO.LOW)
-    print("pumpOn")
-    
 
-def drainOff():
-    GPIO.setup(gpiopins.SOLENOID_PIN, GPIO.OUT) 
-    GPIO.output(gpiopins.SOLENOID_PIN, GPIO.HIGH)
-    
-def ledOn():
-    
-    GPIO.setup(gpiopins.LED_PIN, GPIO.OUT) 
-    GPIO.output(gpiopins.LED_PIN, GPIO.LOW)
-    print("pumpOn")
-    
+def turnOnPump():
+    while distance_sensor.value > 0:
+        pump_up_relay.on()
+    pump_up_relay.off()
 
-def ledOff():
-    GPIO.setup(gpiopins.LED_PIN, GPIO.OUT) 
-    GPIO.output(gpiopins.LED_PIN, GPIO.HIGH)
- 
+def turnOffPump():
+    pump_up_relay.off()
 
+def turnOnLight():
+    grow_light_relay.on()
+
+def turnOffLight():
+    grow_light_relay.off()
+
+def turnOnDrain():
+    while distance_sensor.value <= gpiopins.MAX_DISTANCE:
+        solenoid_valve_relay.on()
+    solenoid_valve_relay.off()
+
+def turnOffDrain():
+    solenoid_valve_relay.off()
 
 def cleanUp():
-    GPIO.cleanup()
+    gpio.GPIODevice.close()
 
-def distance():
+""" def distance():
     temp = tempurature()
     value = Measurement(gpiopins.TRIGGER_PIN, gpiopins.ECHO_PIN, temp, "metric", gpio_mode=GPIO_MODE)
     value = value.raw_distance()
-    return value
+    return value """
 
 def tempurature():
     humidity,temp = Adafruit_DHT.read_retry(DHT22, gpiopins.DHT22_BCM_PIN)
